9070939f02f06bf1fd7490aba92ee05d72dbe545
simon gao <little51@126.com>
2023-04-20 11:39:26 +0800

    fixed json.decoder.JSONDecodeError

--- ChatGLM_6b.py
+++ ChatGLM_6b.py
@@ -2,16 +2,18 @@ import requests
 import json
 import os
 
+
 def getAnswerFromChatGLM6b(context):
     if os.name == 'nt':
         url = 'http://172.16.62.66:8000/stream'
     else:
         url = 'http://172.16.62.136:8000/stream'
-    data = '{"prompt": "'  + context.replace('\r', '').replace('\n', '') +  '", "history": []}'
+    data = '{"prompt": "' + context.replace('\r', '').replace(
+        '\n', '').replace('\\', '').replace('"', '') + '", "history": []}'
     headers = {'content-type': 'application/json;charset=utf-8'}
     r = requests.post(url, data=data.encode(), headers=headers)
     res = r.json()
-    if r.status_code == 200 :
+    if r.status_code == 200:
         return res['response']
     else:
         return ""
